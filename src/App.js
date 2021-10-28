3e19ba2e87393fdbde613fab550b105b77863798
raymankatib <rayman.katib@gmail.com>
2021-10-28 01:29:14 -0400

    wip on filter menue

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Box } from '@chakra-ui/react';
-import './App.css';
 
+import './App.css';
 import SearchBar from './Components/SearchBar';
 import VehiclesGrid from './Components/VehiclesGrid';
+import FilterMenu from './Components/FilterMenu';
 
 function App() {
 	return (
 		<Box p="10px">
 			<Box w="50%" d="flex" justifyContent="space-between">
 				<SearchBar />
+				<FilterMenu />
 			</Box>
 			<VehiclesGrid />
 		</Box>
---
+++ src/Components/FilterMenu.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+	Box,
+	IconButton,
+	Icon,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalCloseButton,
+	ModalBody,
+	useDisclosure,
+	ModalFooter,
+	Button
+} from '@chakra-ui/react';
+import { MdFilterList } from 'react-icons/md';
+
+const FilterMenu = (props) => {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	return (
+		<>
+			<Box onClick={onOpen}>
+				<IconButton ml="5px" icon={<Icon as={MdFilterList} w={6} h={6} />} />
+			</Box>
+			<Modal isOpen={isOpen} onClose={onClose}>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader>Advance filter</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody>{/* <Lorem count={2} /> */}</ModalBody>
+
+					<ModalFooter>
+						<Button colorScheme="red" mr={3} onClick={onClose}>
+							Close
+						</Button>
+						<Button colorScheme="teal">Apply</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+		</>
+	);
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(FilterMenu);
